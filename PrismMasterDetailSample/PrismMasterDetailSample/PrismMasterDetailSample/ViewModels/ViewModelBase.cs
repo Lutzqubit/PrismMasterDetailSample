a088c6b4b82271388656f33ad4da2a1da92094c9
Lutse <lutse@visualstudio.com>
2016-07-27 21:34:02 -0700

    Resources and MainMasterDetailPage ViewModel
    +MainMasterdetailPageViewModel.cs
    +Color.cs
    +StaticUIModels.cs
    +Banner png

---
+++ PrismMasterDetailSample/PrismMasterDetailSample/PrismMasterDetailSample/ViewModels/ViewModelBase.cs
@@ -0,0 +1,16 @@
+﻿using Prism.Commands;
+using Prism.Mvvm;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace PrismMasterDetailSample.ViewModels
+{
+    public class ViewModelBase : BindableBase
+    {
+        public ViewModelBase()
+        {
+
+        }
+    }
+}
