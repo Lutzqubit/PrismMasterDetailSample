7ce50e209fc0554c6eed3dd9c9f578a0ff7e9fb3
Lutse <lutse@visualstudio.com>
2016-08-02 10:53:50 -0700

    More views
    + More views

---
+++ PrismMasterDetailSample/PrismMasterDetailSample/PrismMasterDetailSample/Views/Upcoming.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Xamarin.Forms;
+
+namespace PrismMasterDetailSample.Views
+{
+    public partial class Upcoming : ContentPage
+    {
+        public Upcoming()
+        {
+            InitializeComponent();
+        }
+    }
+}
